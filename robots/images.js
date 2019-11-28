ad087d0cb5c8e172891c38fe5c38df98381c3213
Michael Lean Pedrasoli <pedrasolimichael@gmail.com>
2019-11-27 22:39:14 -0300

    feature: add google images downloader

--- robots/images.js
+++ robots/images.js
@@ -1,20 +1,22 @@
-const state = require('./state')
 const googleapis = require('googleapis').google
 const customSearch = googleapis.customsearch('v1')
+const imagedownloader = require('image-downloader')
 const googleSearchCredentials = require('../credentials/google-search')
+const state = require('./state')
 
 async function robot() {
     const content = state.load()
     await fetchImagesOfAllSentences(content)
-
     state.save(content)
-    
+    await downloadAllImages(content)
+
+
     async function fetchImagesOfAllSentences(content) {
         for (const sentence of content.sentences) {
             const query = `${content.searchTerm} ${sentence.keywords[0]}`
             sentence.images = await fetchGoogleAndReturnImagesLinks(query)
             sentence.googleSearchQuery = query
-        }        
+        }
     }
 
     async function fetchGoogleAndReturnImagesLinks(query) {
@@ -25,11 +27,43 @@ async function robot() {
             searchType: 'image',
             num: 2
         })
+        
+        if (response.data.items) {
+            const imagesUrl = response.data.items.map((item) => {
+                return item.link
+            })
+            return imagesUrl
+        } else {
+            return []
+        }
+        
+    }
 
-        const imagesUrl = response.data.items.map((item) => {
-            return item.link
+    async function downloadAllImages(content) {
+        content.downloadedImages = []
+        for (let sentenceIndex = 0; sentenceIndex < content.sentences.length; sentenceIndex++) {
+            const images = content.sentences[sentenceIndex].images
+            for (let imageIndex = 0; imageIndex < images.length; imageIndex++) {
+                const imageUrl = images[imageIndex];
+                try {
+                    if (content.downloadedImages.includes(imageUrl)) {
+                        throw new Error('Imagem já foi baixada')
+                    }
+                    await downloadAndSave(imageUrl, `${sentenceIndex}-original.png`)
+                    content.downloadedImages = imagesUrl
+                    break
+                } catch (error) {
+                    console.log(`> [${sentenceIndex}][${imageIndex}] Erro ao baixar (${imageUrl}): ${error}`)
+                }
+            }
+        }
+    }
+
+    async function downloadAndSave(url, filename) {
+        return imagedownloader.image({
+            url: url,
+            dest: `./content/${filename}`
         })
-        return imagesUrl
     }
 }
 
